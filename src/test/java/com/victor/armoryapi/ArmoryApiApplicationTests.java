9dd2fa0e1d4cdad4191b8f81aefeaeb98ff29e63
victor <prkone@hotmail.fr>
2024-11-15 16:17:29 +0100

    first

---
+++ src/test/java/com/victor/armoryapi/ArmoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.armoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
